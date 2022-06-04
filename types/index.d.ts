28c310a1081e5dea6ae54849ce493a3e0a63c0ef
j4ck <50369519+j4cegh@users.noreply.github.com>
2022-06-04 19:33:48 +0300

    aiosdjas

---
+++ types/index.d.ts
@@ -0,0 +1,10 @@
+export = Database;
+declare class Database {
+    constructor(url: any);
+    ws: any;
+    queue: any[];
+    connect(): Promise<any>;
+    get(tableName: any, select: any, key: any, value: any): Promise<any>;
+    set(tableName: any, key: any, value: any): Promise<any>;
+    add(tableName: any, key: any, value: any): Promise<any>;
+}
